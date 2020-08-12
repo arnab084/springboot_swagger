6d2733ca9e0b2d6036c50f0fa3528e268bf0a7e5
Arnab Chakraborty <arnab.chakraborty@optimizeitsystems.com>
2020-08-12 22:46:09 +0530

    Updated

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HelloController {
-
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+	
+	@GetMapping(path="/getBestUser")
+	  public @ResponseBody User getBestUser() {
+		return new User("arnab","arnabp");
+	  }
+	
+	@GetMapping(path="/getAllUsers")
+	  public @ResponseBody User[] getAllUsers() {
+		User[] obj = new User[2];
+		obj[0] = new User("arnab","arnabp");
+		obj[1] = new User("arnab2","arnabp");
+		return obj;
+	  }
 
 }
